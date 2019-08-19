0838ddb5b34297140de1fb0187613f50760b5bc9
cocoitali <cmburatto@gmail.com>
2019-08-19 15:44:10 -0600

    Wrote first test, played around with code, added more tests to catch loopholes

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,12 @@
-const { add } = require('./calculator.js');
+const { add } = require("./calculator.js");
 
-// test away!
+describe("the add function", () => {
+  it("it should add two values", () => {
+    //set up tests here
+    const result = add(2, 2);
+    //assertion here
+    expect(result).toBe(4);
+    expect(add(-1,4)).toBe(3);
+    expect(add(0,6)).toBe(6);
+  });
+});
